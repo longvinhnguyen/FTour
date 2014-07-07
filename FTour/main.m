cad6e2a699bd706ae41075041e998c528495d01a
LongNV <long.omni@gmail>
2014-07-07 16:08:16 +0700

    Initial commit FTour project

---
+++ FTour/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FTour
+//
+//  Created by LongNV on 7/7/14.
+//  Copyright (c) 2014 FSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ FTourTests/FTourTests.m
@@ -0,0 +1,34 @@
+//
+//  FTourTests.m
+//  FTourTests
+//
+//  Created by LongNV on 7/7/14.
+//  Copyright (c) 2014 FSoft. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FTourTests : XCTestCase
+
+@end
+
+@implementation FTourTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
