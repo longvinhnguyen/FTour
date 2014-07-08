9506c9a9d68b668e56f1aff8a411442696faf2c1
KhanhhoaMai <trung.nguyen193@gmail.com>
2014-07-08 08:51:26 +0700

    add design

---
+++ FTour/FTour/FTour/WelcomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  WelcomeViewController.h
+//  FTour
+//
+//  Created by Khanhhoa Mai on 7/8/14.
+//  Copyright (c) 2014 FSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WelcomeViewController : UIViewController
+
+@end
---
+++ FTour/FTour/FTour/iBeaconViewController.h
@@ -0,0 +1,13 @@
+//
+//  iBeaconViewController.h
+//  FTour
+//
+//  Created by Khanhhoa Mai on 7/8/14.
+//  Copyright (c) 2014 FSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iBeaconViewController : UIViewController
+
+@end
