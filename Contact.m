c38fadc6e6c4194facd75d8355d6a5889ea662ac
KhanhhoaMai <trung.nguyen193@gmail.com>
2014-08-04 13:17:21 +0700

    add model

---
+++ Contact.m
@@ -0,0 +1,13 @@
+//
+//  Contact.m
+//  FTour
+//
+//  Created by Khanhhoa Mai on 8/4/14.
+//  Copyright (c) 2014 FSoft. All rights reserved.
+//
+
+#import "Contact.h"
+
+@implementation Contact
+
+@end
